209b2c76cca07ac9b7a0416dea297db64b86f651
Nanda Scott <nanda1123@gmail.com>
2015-06-16 10:59:49 -0400

    Added gems, created Pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
   resources :users
   resources :images
   # The priority is based upon order of creation: first created -> highest priority.
